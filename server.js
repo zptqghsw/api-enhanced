b9334d58e4513be6ceef3baf0db329168a6ba685
zptqghsw <83914995+zptqghsw@users.noreply.github.com>
2026-06-04 13:10:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -331,7 +331,7 @@ async function constructServer(moduleDefs) {
 
         // 夹带私货部分：如果开启了通用解锁，并且是获取歌曲URL的接口，则尝试解锁（如果需要的话）ヾ(≧▽≦*)o
         if (
-          req.baseUrl === '/song/url/v1'
+          req.originalUrl.split('?')[0] === '/song/url/v1'
         ) {
           const song = moduleResponse.body.data[0]
           if (
